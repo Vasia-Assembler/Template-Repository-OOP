f64fe6acecbc81cf10a4dad31f03e202856b6f76
maximbry <154349684+maximbry@users.noreply.github.com154349684+maximbry@users.noreply.github.com>
2024-04-01 13:48:12 +0300

    init lab-2

---
+++ OOP-ICT.Second/Models/Player.cs
@@ -0,0 +1,12 @@
+﻿namespace OOP_ICT.Second.Models;
+public class Player
+{
+    public string Name { get; }
+    public Account Account { get; }
+
+    public Player(string name, Account account)
+    {
+        Name = name;
+        Account = account;
+    }
+}
